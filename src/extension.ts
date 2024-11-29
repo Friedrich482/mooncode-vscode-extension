65f6ffd5f8cffdb39457e118808d6395b0900eb6
Friedrich482 <friedricht109@gmail.com>
2024-11-29 23:12:01 +0100

    feat: getTime
    used one function to get the time elapsed since the beginning of the counter and also the time elapsed between two keystrokes
    
    The time elapsed since the beginning should be stopped if there is no activity in 15 minutes

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,19 @@
 import * as vscode from "vscode";
-import getElapsedTime from "./utils/getElapsedTime";
+import getTime from "./utils/getTime";
 
-// This method is called when your extension is activated
 export function activate(context: vscode.ExtensionContext) {
-  // This line of code will only be executed once when your extension is activated
-
-  vscode.window.showInformationMessage(
-    "MoonCode starts now tracking your code time"
-  );
-  getElapsedTime();
-
-  // Now provide the implementation of the command with registerCommand
-  // ! The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand(
-    "MoonCode.start",
-    () => {}
-  );
+  const disposable = vscode.commands.registerCommand("MoonCode.start", () => {
+    vscode.window.showInformationMessage(
+      "MoonCode starts now tracking your code time"
+    );
+    getTime();
+  });
 
   context.subscriptions.push(disposable);
+
+  vscode.commands.executeCommand("MoonCode.start");
 }
 
-// This method is called when your extension is deactivated
 export async function deactivate() {
   await new Promise((resolve) => setTimeout(resolve, 5000));
   console.log("MoonCode deactivated");
